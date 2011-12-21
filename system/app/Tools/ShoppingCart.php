b2c9c07b58f0dec2f790fa78b3e23b80fbeba3f7
Pavel Kovalyov <sxe.funky@gmail.com>
2011-12-21 11:58:19 +0200

    added cart handler

---
+++ system/app/Tools/ShoppingCart.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * ShoppingCart
+ * @author Pavel Kovalyov <pavlo.kovalyov@gmail.com>
+ */
+class Tools_ShoppingCart {
+
+    protected static $_instance;
+
+    private $_content;
+
+    private $_session;
+
+    private function __construct() {
+        $this->_websiteHelper   = Zend_Controller_Action_HelperBroker::getExistingHelper('website');
+        $this->_shoppingConfig  = Models_Mapper_ShoppingConfig::getInstance()->getConfigParams();
+        if ($this->_session === null){
+            $this->_session = new Zend_Session_Namespace($this->_websiteHelper->getUrl().'cart');
+        }
+        $this->_content = new SplObjectStorage();
+        $this->_load();
+    }
+
+    private function __clone() { }
+
+    private function __wakeup() { }
+
+    /**
+     * Returns instance of Shopping Cart
+     * @return Tools_ShoppingCart
+     */
+    public static function getInstance() {
+        if (is_null(self::$_instance)){
+            self::$_instance = new Tools_ShoppingCart();
+        }
+
+        return self::$_instance;
+    }
+
+    private function _load() {
+        if (isset($this->_session->cartContent)) {
+            $this->_content = unserialize($this->_session->cartContent);
+        }
+
+        return $this;
+    }
+
+    private function _save() {
+        $this->_session->cartContent = serialize($this->_content);
+    }
+
+    public function setContent($content)
+    {
+        $this->_content = $content;
+    }
+
+    public function getContent()
+    {
+        return $this->_content;
+    }
+
+    public function add($item)
+    {
+
+    }
+
+    public function remove($item)
+    {
+
+    }
+
+}
