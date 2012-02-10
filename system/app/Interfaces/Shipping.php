56862ad81f0708159054ecb688743a1243df465e
Eugene NE NEzhuta <theneiam@gmail.com>
2012-02-10 17:24:14 +0200

    - shipping interface updates

--- system/app/Interfaces/Shipping.php
+++ system/app/Interfaces/Shipping.php
@@ -5,5 +5,26 @@
  * @author Eugene I. Nezhuta <eugene@seotoaster.com>
  */
 interface Interfaces_Shipping {
+
+	/**
+	 * Setting origination address from array
+	 */
+	public function setOrigination(array $address);
+
+	/**
+     * Setting destination address from array
+     */
+	public function setDestination(array $address);
+
+	/**
+     * Setting weight and units
+     */
+    public function setWeight($weight, $unit = '');
+
+	/**
+     * Main method to interact with shipper API server and return a result
+     */
+    public function run();
+
 	public static function getConfigScreen();
 }
