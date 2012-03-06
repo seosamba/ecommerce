272e3f30c3f64244a3934e6242e745ca0d5bda0c
Pavel Kovalyov <sxe.funky@gmail.com>
2012-03-06 19:08:22 +0200

    added AddressChain jQuery plugin

---
+++ web/js/libs/jquery/addresschain.jquery.js
@@ -0,0 +1,81 @@
+/*!
+ * jQuery address chain plugin
+ * Author: Pavlo Kovalov <pavlo.kovalyov@gmail.com>
+ * Licensed under the MIT license
+ */
+
+;(function ( $, window, document, undefined ) {
+
+    $.fn.addressChain = function ( options ) {
+        options = $.extend( {}, $.fn.addressChain.options, options );
+
+        return this.each(function () {
+            var elem = $(this),
+                countrySelect = (options.countrySelector instanceof $ ? options.countrySelector : $(options.countrySelector, this)),
+                stateSelect   = (options.stateSelector instanceof $ ? options.stateSelector : $(options.stateSelector, this))
+                stateLabel = stateSelect.attr('id') && $('label[for='+stateSelect.attr('id')+']');
+
+            //hiding state element if it's empty
+            if (stateSelect.find('option').length === 0){
+                stateSelect.hide();
+                stateLabel && stateLabel.hide();
+            }
+
+            countrySelect.on('change', function(){
+                var countryCode = $(this).val();
+
+                //doing AJAX-request
+                $.ajax({
+                    url: options.url,
+                    dataType: 'json',
+                    data: { country: countryCode },
+                    success: function(response) {
+                        if (response) {
+
+                            var html = '';
+
+                            $.each(response, function(key, item){
+                               //handling if states returned as key-value pairs of as set of objects
+                                if (typeof item === 'string') {
+                                    html += '<option value="' + key + '" label="' + item +
+                                        '" data-country="' + countryCode + '">' + item + '</option>';
+                                } else if (typeof item === 'object') {
+                                    html += '<option value="' + this.id + '" label="' + this.name +
+                                        '" data-country="' + this.country + '">' + this.name + '</option>';
+                                } else {
+                                    $.error('Wrong response data format from server');
+                                }
+                            });
+                            stateSelect.html(html);
+
+                            if (html.length) {
+                                stateSelect.show();
+                                stateLabel && stateLabel.show();
+                            } else {
+                                stateSelect.hide();
+                                stateLabel && stateLabel.hide();
+                            }
+                        }
+                    }
+                });
+            });
+        });
+    };
+
+    // Globally overriding options
+    // Here are our publicly accessible default plugin options
+    // that are available in case the user doesn't pass in all
+    // of the values expected. The user is given a default
+    // experience but can also override the values as necessary.
+    // eg. $fn.pluginName.key ='otherval';
+
+    
+    $.fn.addressChain.options = {
+        countrySelector: "select[name$=country]",
+        stateSelector: "select[name$=state]",
+        countryUrl: null,
+        url: '',
+        cache: false
+    };
+
+})( jQuery, window, document );
