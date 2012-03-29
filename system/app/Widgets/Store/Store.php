3d92837de28208c6f86e29becb7344306acee3be
Pavel Kovalyov <sxe.funky@gmail.com>
2012-03-29 17:14:18 +0300

    allowed option fetching using reflection

--- system/app/Widgets/Store/Store.php
+++ system/app/Widgets/Store/Store.php
@@ -17,4 +17,22 @@ class Widgets_Store_Store extends Widgets_Abstract {
         }
         return false;
     }
+
+	public static function getAllowedOptions() {
+		$cartPluginName = Models_Mapper_ShoppingConfig::getInstance()->getConfigParam('cartPlugin');
+		$reflection = new Zend_Reflection_Class(ucfirst($cartPluginName));
+		$methods = $reflection->getMethods();
+
+		$allowedOptions = array();
+		foreach ($methods as $method) {
+			if (strpos($method->getName(), Tools_Plugins_Abstract::OPTION_MAKER_PREFIX) !== false){
+				$name = str_replace(Tools_Plugins_Abstract::OPTION_MAKER_PREFIX,'', $method->getName());
+				array_push($allowedOptions, array(
+					'alias'  => 'Store' .' '. $name,
+					'option' => 'store:'.strtolower($name)
+				));
+			}
+		}
+		return $allowedOptions;
+	}
 }
