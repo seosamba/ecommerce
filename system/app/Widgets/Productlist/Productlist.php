ecf25248d42cad8d805d674e98a789de39e09ef1
Eugene NE NEzhuta <theneiam@gmail.com>
2012-04-02 12:47:38 +0300

    - Template search fixed

--- system/app/Widgets/Productlist/Productlist.php
+++ system/app/Widgets/Productlist/Productlist.php
@@ -74,7 +74,7 @@ class Widgets_Productlist_Productlist extends Widgets_Abstract {
 		$this->_view->limit      = self::DEFAULT_OFFSET;
         $this->_websiteHelper    = Zend_Controller_Action_HelperBroker::getExistingHelper('website');
 	    $this->_productMapper    = Models_Mapper_ProductMapper::getInstance();
-		$this->_productTemplate  = Application_Model_Mappers_TemplateMapper::getInstance()->findByName(array_shift($this->_options));
+		$this->_productTemplate  = Application_Model_Mappers_TemplateMapper::getInstance()->find(array_shift($this->_options));
 		if($this->_productTemplate === null) {
 			throw new Exceptions_SeotoasterWidgetException('Product template doesn\'t exist');
 		}
