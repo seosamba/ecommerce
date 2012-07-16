56aa56b7f9b09ca20ea1970ecf6cdc0f20bc60e4
Pavel Kovalyov <sxe.funky@gmail.com>
2012-07-16 19:26:53 +0300

    added paginator calculation based on X-Toasted-Total-Rows header sent from server added getFilter() function to get hash of current applied filters

--- web/js/modules/manage-products/collections/productlist.js
+++ web/js/modules/manage-products/collections/productlist.js
@@ -15,23 +15,42 @@ define([
         paginator_ui: {
             firstPage: 0,
             currentPage: 0,
-            perPage: 30
-//            totalPages: 2
+            perPage: 20
         },
         server_api: {
             'key': function() { return $('input[name="productsearch"]').val(); },
             'limit': function() { return this.perPage },
             'offset': function() { return this.currentPage * this.perPage },
             'order': '',
+            'count': true,
             'ftag': function() { return $('select[name="filter-tag"]').val() || ''; },
             'fbrand': function() { return $('select[name="filter-brand"]').val() || ''; }
         },
-        parse: function (response) {
-            var resultsCount = _.size(response);
+        parse: function (response, xhr) {
+            console.log(response);
+            var resultsCount = _.size(response),
+                totalCount = xhr.getResponseHeader('X-Toasted-Total-Rows');
+            if (!isNaN(totalCount)){
+                this.totalPages = Math.floor(totalCount/this.perPage);
+                this.totalCount = totalCount;
+            }
             if (resultsCount < this.perPage){
                 this.totalPages = this.currentPage;
             }
             return response;
+        },
+        getFilter: function(){
+            var self = this,
+                params = {};
+            _.each(self.server_api, function(value, key){
+                if( _.isFunction(value) ) {
+                    value = _.bind(value, self);
+                    value = value();
+                }
+                params[key] = value;
+            });
+
+            return params;
         }
     });
 
