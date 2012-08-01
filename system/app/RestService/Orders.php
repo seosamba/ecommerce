8129847dbdc58c0709a1b54477aa9c028449691b
Pavel Kovalyov <sxe.funky@gmail.com>
2012-08-01 19:09:19 +0300

    RestService initial commit

---
+++ system/app/RestService/Orders.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Rest_OrdersService
+ * @author Pavel Kovalyov <pavlo.kovalyov@gmail.com>
+ */
+class RestService_Orders extends RestService_Abstract {
+
+	/**
+	 * The get action handles GET requests and receives an 'id' parameter; it
+	 * should respond with the server resource state of the resource identified
+	 * by the 'id' value.
+	 */
+	public function getAction() {
+		$this->_responseHelper->response(array('pacman' => 'waka'));
+	}
+
+	/**
+	 * The post action handles POST requests; it should accept and digest a
+	 * POSTed resource representation and persist the resource state.
+	 */
+	public function postAction() {
+		// TODO: Implement postAction() method.
+	}
+
+	/**
+	 * The put action handles PUT requests and receives an 'id' parameter; it
+	 * should update the server resource state of the resource identified by
+	 * the 'id' value.
+	 */
+	public function putAction() {
+		// TODO: Implement putAction() method.
+	}
+
+	/**
+	 * The delete action handles DELETE requests and receives an 'id'
+	 * parameter; it should update the server resource state of the resource
+	 * identified by the 'id' value.
+	 */
+	public function deleteAction() {
+		// TODO: Implement deleteAction() method.
+	}
+
+
+}
