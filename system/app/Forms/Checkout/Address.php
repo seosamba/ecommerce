f333a11fc7073ce641d0cf2c7d73778cee77a894
Eugene Nezhuta <theneiam@gmail.com>
2012-09-21 18:13:10 +0300

    Submit button lable changed from "Checkout" to "Next"

--- system/app/Forms/Checkout/Address.php
+++ system/app/Forms/Checkout/Address.php
@@ -79,7 +79,7 @@ class Forms_Checkout_Address extends Forms_Address_Abstract {
 		$this->addElement(new Zend_Form_Element_Submit(array(
 			'name'   => 'checkout',
 			'ignore' => true,
-			'label'  => 'Checkout',
+			'label'  => 'Next',
 			'decorators' => array('ViewHelper')
 		)));
 
