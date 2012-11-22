8d3cbb1a327137360b4d0295ff617473072d8734
AndreyBudchenko <andrei@seosamba.com>
2012-11-22 11:11:27 +0200

    fixes for wording

--- system/app/Forms/Checkout/Address.php
+++ system/app/Forms/Checkout/Address.php
@@ -25,7 +25,7 @@ class Forms_Checkout_Address extends Forms_Address_Abstract {
         $websiteUrl = Zend_Controller_Action_HelperBroker::getExistingHelper('website')->getUrl();
         
         $termsAndConditionsPage = Application_Model_Mappers_PageMapper::getInstance()->fetchByOption(Shopping::OPTION_STORE_SHIPPING_TERMS);
-        $notesLabel = 'I authorize the parsel to be left at the delivery address without signature.';
+        $notesLabel = 'I authorize the parcel to be left at the delivery address without signature.';
         if(!empty($termsAndConditionsPage)){
 	        $termsAndConditionsPage = current($termsAndConditionsPage);
             $notesLabel .= ' <a href="'.$websiteUrl.$termsAndConditionsPage->getUrl().'" target = _blank class="terms-page" title="Shipping Policy">Shipping Policy</a>';
@@ -42,7 +42,7 @@ class Forms_Checkout_Address extends Forms_Address_Abstract {
         
         $this->addElement(new Zend_Form_Element_Textarea(array(
 			'name'     => 'notes',
-			'label'    => 'For delivery comments',
+			'label'    => 'Delivery Comments',
             'rows'     => '3',
             'cols'     => '45'
 		)));
