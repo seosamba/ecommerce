51a3e4a7a8f5f0ba0e89b7e3ecf41fad4c234af5
andreybudchenko <andrei@seosamba.com>
2013-11-14 13:11:28 +0200

    removed not useful statuses from customer grid

--- system/app/Widgets/User/User.php
+++ system/app/Widgets/User/User.php
@@ -125,37 +125,19 @@ class Widgets_User_User extends Widgets_Abstract {
         $orders = Models_Mapper_CartSessionMapper::getInstance()->fetchAll(array('user_id = ?' => $this->_customer->getId()));
         $this->_view->stats = array(
             'all'     => sizeof($orders),
-            'new'     => sizeof(array_filter($orders, function ($order) {
-                    return (!$order->getStatus() || ($order->getStatus() === Models_Model_CartSession::CART_STATUS_NEW));
-                })
-            ),
             'completed' => sizeof(array_filter($orders, function ($order) {
                 return $order->getStatus() === Models_Model_CartSession::CART_STATUS_COMPLETED;
             })),
-            'pending'   => sizeof(array_filter($orders, function ($order) {
-                return ($order->getStatus() === Models_Model_CartSession::CART_STATUS_PENDING && $order->getGateway() !== 'Quote');
-            })),
-            //'processing'   => sizeof(array_filter($orders, function ($order) {
-                //return ($order->getStatus() === Models_Model_CartSession::CART_STATUS_PROCESSING && $order->getGateway() !== self::GATEWAY_QUOTE);
-            //})),
-            //'canceled'   => sizeof(array_filter($orders, function ($order) {
-                //return ($order->getStatus() === Models_Model_CartSession::CART_STATUS_CANCELED && $order->getGateway() !== self::GATEWAY_QUOTE);
-            //})),
             'shipped'   => sizeof(array_filter($orders, function ($order) {
                 return $order->getStatus() === Models_Model_CartSession::CART_STATUS_SHIPPED;
             })),
             'delivered' => sizeof(array_filter($orders, function ($order) {
                 return $order->getStatus() === Models_Model_CartSession::CART_STATUS_DELIVERED;
             })),
-            'new_quote' => sizeof(array_filter($orders, function ($order) {
-                return ($order->getStatus() === Models_Model_CartSession::CART_STATUS_PENDING && $order->getGateway() === 'Quote');
-            })),
             'quote_sent' => sizeof(array_filter($orders, function ($order) {
                 return ($order->getStatus() === Models_Model_CartSession::CART_STATUS_PROCESSING && $order->getGateway() === 'Quote');
             }))
-            //'lost_opportunity' => sizeof(array_filter($orders, function ($order) {
-                //return ($order->getStatus() === Models_Model_CartSession::CART_STATUS_CANCELED && $order->getGateway() === self::GATEWAY_QUOTE);
-            //}))
+
         );
         $this->_view->orders = $orders;
         return $this->_view->render('grid.phtml');
