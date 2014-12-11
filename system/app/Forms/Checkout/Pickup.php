2b1775c82f57f88ba476b6b2779d1ca6787d87a9
Oleg Malii <oleg@seotoaster.com>
2014-12-11 17:13:27 +0200

    Fix mobile phone numbers

--- system/app/Forms/Checkout/Pickup.php
+++ system/app/Forms/Checkout/Pickup.php
@@ -5,13 +5,40 @@
  */
 class Forms_Checkout_Pickup extends Zend_Form {
 
+    protected $_mobilecountrycode = null;
+
+    protected $_mobile = null;
+
+    public function setMobilecountrycode($_mobilecountrycode)
+    {
+        $this->_mobilecountrycode = $_mobilecountrycode;
+        $this->getElement('mobilecountrycode')->setValue($this->_mobilecountrycode);
+    }
+
+    public function getMobilecountrycode()
+    {
+        return $this->_mobilecountrycode;
+    }
+
+    public function setMobile($_mobile)
+    {
+        $countryPhoneCode = Zend_Locale::getTranslation($this->_mobilecountrycode, 'phoneToTerritory');
+        $this->_mobile = preg_replace('/^(\+' . $countryPhoneCode . ')(\d+)/', '$2', $_mobile);
+        $this->getElement('mobile')->setValue($this->_mobile);
+    }
+
+    public function getMobile()
+    {
+        return $this->_mobile;
+    }
+
 	public function init(){
 		parent::init();
 
 		$this->setLegend('Enter pick up information')
             ->setAttribs(array(
             'id'     => 'checkout-pickup',
-            'class'  => array('toaster-checkout'),
+            'class'  => 'toaster-checkout',
             'method' => Zend_Form::METHOD_POST
         ));
 
@@ -34,14 +61,26 @@ class Forms_Checkout_Pickup extends Zend_Form {
         $this->addElement(new Zend_Form_Element_Text(array(
             'name'     => 'phone',
             'label'    => 'Phone',
-            'value'    => '+'.Zend_Locale::getTranslation(Models_Mapper_ShoppingConfig::getInstance()->getConfigParam('country'), 'phoneToTerritory')
+            'value'    => ''
 //            'required' => true
+        )));
+
+        $this->addElement(new Zend_Form_Element_Select(array(
+            'name'         => 'mobilecountrycode',
+            'label'        => null,
+            'multiOptions' => Tools_System_Tools::getCountryPhoneCodesList(),
+            'value'        => $this->_mobilecountrycode,
+            'style'        => 'width: 41.667%;'
         )));
 
         $this->addElement(new Zend_Form_Element_Text(array(
                 'name'     => 'mobile',
-                'label'    => 'Mobile'
+                'label'    => null,
+                'style'    => 'width: 58.333%;',
+                'value'    => $this->_mobile
         )));
+
+
 
         $this->addElement(new Zend_Form_Element_Text(array(
             'name'       => 'email',
@@ -76,6 +115,8 @@ class Forms_Checkout_Pickup extends Zend_Form {
         ));
 
         $this->getElement('submitpickup')->removeDecorator('Label');
+        $this->getElement('mobilecountrycode')->removeDecorator('HtmlTag');
+        $this->getElement('mobile')->removeDecorator('HtmlTag');
 	}
 
 }
