60ad99c108dbc2c0593119c28caffcc44a82cc8b
Vitaly-V <gtopbox@gmail.com>
2015-02-16 15:07:13 +0200

    Add alters for paypal

--- system/store-alters.sql
+++ system/store-alters.sql
@@ -115,5 +115,62 @@ UPDATE `template_type` SET `title` = 'Product' WHERE `id` = 'typeproduct';
 ALTER TABLE `shopping_customer_address` ADD `mobilecountrycode` VARCHAR( 2 ) NULL DEFAULT NULL COMMENT 'Contains mobile phone country code';
 INSERT INTO `plugin` (`name`, `status`, `tags`, `version`) VALUES ('delivery', 1, '', '2.2.0');
 
+CREATE TABLE IF NOT EXISTS `plugin_paypal_settings` (
+  `id`           INT(10)      NOT NULL,
+  `email`        VARCHAR(255) NOT NULL,
+  `apiSignature` VARCHAR(255) NOT NULL,
+  `apiUser`      VARCHAR(255) NOT NULL,
+  `apiPassword`  VARCHAR(255) NOT NULL,
+  `useSandbox`   TINYINT(1)   NOT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  CHARACTER SET utf8
+  COLLATE utf8_unicode_ci;
+
+INSERT INTO `plugin_paypal_settings` VALUES ('1', '', '', '', '', '0')
+ON DUPLICATE KEY UPDATE id = 1;
+
+CREATE TABLE IF NOT EXISTS `plugin_paypal_transactions` (
+  `id`                      INT          NOT NULL AUTO_INCREMENT,
+  `txnId`                   VARCHAR(200) NULL,
+  `payerId`                 VARCHAR(200) NULL,
+  `payerMail`               VARCHAR(150) NULL,
+  `amount`                  VARCHAR(25)  NULL,
+  `shippingAmount`          VARCHAR(25)  NULL,
+  `tax`                     VARCHAR(25)  NULL,
+  `currency`                TEXT         NULL,
+  `paymentStatus`           TEXT         NULL,
+  `status`                  TEXT         NULL,
+  `paymentType`             TEXT         NULL,
+  `paymentId`               INT          NULL,
+  `paymentDate`             TIMESTAMP    NULL     DEFAULT NULL,
+  `pFirstName`              VARCHAR(50)  NULL,
+  `pLastName`               VARCHAR(50)  NULL,
+  `pCountry`                VARCHAR(60)  NULL,
+  `pCountryCode`            TEXT         NULL,
+  `pAddressState`           VARCHAR(60)  NULL,
+  `pAddressCity`            VARCHAR(60)  NULL,
+  `pAddressZip`             TEXT         NULL,
+  `pAddressName`            VARCHAR(150) NULL,
+  `cartId`                  INT(10)      NULL,
+  `pendingReason`           VARCHAR(50)  NULL,
+  `subscribeStatus`         VARCHAR(60)           DEFAULT NULL,
+  `subscribePeriod`         INT(10)               DEFAULT NULL,
+  `subscribePeriodType`     VARCHAR(50)           DEFAULT NULL,
+  `subscribeQuantity`       INT(10)               DEFAULT NULL,
+  `subscribeAmount`         DECIMAL(10, 4)        DEFAULT NULL,
+  `subscribeDate`           VARCHAR(60)           DEFAULT NULL,
+  `subscriptionId`          VARCHAR(255)          DEFAULT NULL,
+  `subscriptionDatePayed`   VARCHAR(60)           DEFAULT NULL,
+  `subscriptionAmountPayed` VARCHAR(60)           DEFAULT NULL,
+  `emailSent`               ENUM('0', '1')        DEFAULT '0',
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  CHARACTER SET utf8
+  COLLATE utf8_unicode_ci;
+INSERT INTO `plugin` (`name`, `status`, `tags`, `version`) VALUES ('paypal', 1, '', '2.2.3');
+
 UPDATE `plugin` SET `version`='2.4.0' WHERE `name`='shopping';
 UPDATE `shopping_config` SET `value`='2.4.0' WHERE `name`='version';
