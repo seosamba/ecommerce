1c871d9c8d60e64102181b3a7c588013feb9db0f
Vitaly-V <gtopbox@gmail.com>
2015-08-06 13:05:36 +0000

    Fix version

--- system/install.sql
+++ system/install.sql
@@ -784,4 +784,4 @@ CREATE TABLE IF NOT EXISTS `shopping_coupon_sales` (
 
 INSERT INTO `page_types` (`page_type_id`, `page_type_name`) VALUES ('2', 'product');
 
-UPDATE `plugin` SET `version` = '2.4.3' WHERE `name` = 'shopping';
+UPDATE `plugin` SET `version` = '2.4.4' WHERE `name` = 'shopping';
