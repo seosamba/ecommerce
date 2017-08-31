c524c99fe87b9d9588201f45f6f39e21f3c41c41
andreybudchenko <andrei@seosamba.com>
2017-08-31 18:32:00 +0300

    Fix state name

--- system/app/Widgets/Postpurchase/Postpurchase.php
+++ system/app/Widgets/Postpurchase/Postpurchase.php
@@ -822,11 +822,7 @@ class Widgets_Postpurchase_Postpurchase extends Widgets_Abstract
      * @return string
      */
     protected function getState($stateId) {
-        $state = Tools_Geo::getStateById($stateId);
-        if (!empty($state['state'])) {
-            return $state['state'];
-        }
-        return '';
+        return Tools_Geo::getStateByParam($stateId);
     }
 
 }
