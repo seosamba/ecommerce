bd1a0a29dbfebd7e1b2390a5f91f12ba6beb7f52
andreybudchenko <andrei@seosamba.com>
2019-12-19 16:10:21 +0200

    Update vue libraries

--- web/js/vue-modules/webpack.config.js
+++ web/js/vue-modules/webpack.config.js
@@ -61,12 +61,6 @@ if (process.env.NODE_ENV === 'production') {
                 NODE_ENV: '"production"'
             }
         }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            compress: {
-                warnings: false
-            }
-        }),
         new webpack.LoaderOptionsPlugin({
             minimize: true
         })
