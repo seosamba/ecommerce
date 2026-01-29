f0c8664f11ff55f20d5c2c68c2aa9f46404ad552
Andrey Budchenko <andrei@seosamba.com>
2026-01-29 17:41:58 +0200

    Add support for product preview convert to webp

--- web/js/modules/product/main.js
+++ web/js/modules/product/main.js
@@ -9,12 +9,16 @@ define([ './views/app' ], function(AppView){
     window.app = new AppView();
     $(function(){
         $(document).trigger('loaded.product');
-        jsProductTeaserUploader.bind('FileUploaded', function(uploader, file){
-            newSrc = $('#website_url').val()+'media/products/small/'+file.name.replace(/\s+/g, '-').toLowerCase();
-            window.app.model.set('photo', 'products/' + file.name.replace(/\s+/g, '-').toLowerCase());
-            $('#product-image').attr('src', newSrc);
-            window.app.trigger('change');
-            $('#progressbar').delay(800).fadeOut();
+        jsProductTeaserUploader.bind('FileUploaded', function(uploader, file, info){
+            var response = jQuery.parseJSON(info.response);
+            if (typeof response.source !== 'undefined') {
+                var newFileName = response.source.split(/[\\/]/).pop();
+                var newSrc = $('#website_url').val()+'media/products/small/'+newFileName.toLowerCase();
+                window.app.model.set('photo', 'products/' + newFileName.toLowerCase());
+                $('#product-image').attr('src', newSrc);
+                window.app.trigger('change');
+                $('#progressbar').delay(800).fadeOut();
+            }
         });
     });
 
